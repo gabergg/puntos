c505f1c0b9cde39485db5205ca37339dbb7950f8
Gabe G'Sell <gabergg@gmail.com>
2014-08-07 21:39:05 -0700

    initial repo, RoRT gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Puntos::Application.config.secret_key_base = 'd36b9320c1eb05b441bea2f97fa692201b0c32a2aad5e875555905649bb33aebb01d174d3482fc6c19c1e87cc9d4644d4547b898e29b3dabba47d9a3e5745338'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Puntos::Application.config.session_store :cookie_store, key: '_puntos_session'
