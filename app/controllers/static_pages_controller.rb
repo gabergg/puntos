1131bfca6863debeb44eee7b9b38083678ac330e
Gabe G'Sell <gabergg@gmail.com>
2014-08-18 23:54:25 -0700

    build daily habit for testing form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @habit = current_user.habits.build
-      @daily_habit = current_user.daily_list(Date.current).first
+      #@daily_habit = current_user.daily_list(Date.current).first
+      @daily_habit = current_user.daily_habits.build
     end
   end
 
