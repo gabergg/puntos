da010715659c1e399a0708a4058a9bb22f4414de
Gabe G'Sell <gabergg@gmail.com>
2014-08-21 23:27:18 -0700

    nil session date = today

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @habit = current_user.habits.build
-      @daily_items = DailyHabit.daily_list(current_user.id, session[:date])
+      @daily_items = DailyHabit.daily_list(current_user.id, session[:date] || Date.today)
     end
   end
 
